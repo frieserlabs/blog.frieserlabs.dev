9c5bc0c3cd27730a0052497e1c3c48eff1a9ccc2
hector.molano <hector.molano.next@bbva.com>
2022-01-10 12:46:21 +0100

    preload css

---
+++ public/js/toggleBorder.min.b969b1f4f273e794109349fe2d8ed61a.js
@@ -0,0 +1,1 @@
+window.addEventListener("resize",adjustListBorder);let listRemoveWidth=window.matchMedia("(max-width: 1070px)"),listAddWidth=window.matchMedia("(min-width: 1070px)");function adjustListBorder(){listRemoveWidth.matches?document.getElementById("articlesList").classList.remove("author-alc"):listAddWidth.matches&&document.getElementById("articlesList").classList.add("author-alc")}
